474d7efa397fa074fdc37900f03331530eab7c81
Frost <159105703+Karan-Frost@users.noreply.github.com>
2024-12-09 21:29:03 -0500

    veux: Move to Xiaomi IR AIDL
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>

--- veux-vendor.mk
+++ veux-vendor.mk
@@ -801,7 +801,6 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl-qti \
     camera.qcom \
     com.qti.chi.override \
-    consumerir.holi \
     fingerprint.fpc.default \
     fingerprint.goodix.default \
     fingerprint.silead.default \
